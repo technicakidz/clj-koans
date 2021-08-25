2673826d41a54fc0850f648f1a925a33927f98ea
technicakidz <y.technicakidz08@gmail.com>
2021-08-25 22:44:25 +0900

    15: completed

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -7,41 +7,46 @@
    :state "TX"})
 
 (meditations
-  "Destructuring is an arbiter: it breaks up arguments"
-  (= ":bar:foo" ((fn [[a b]] (str b a))
-         [:foo :bar]))
+ "Destructuring is an arbiter: it breaks up arguments"
+ (= ":bar:foo" ((fn [[a b]] (str b a))
+                [:foo :bar]))
 
-  "Whether in function definitions"
-  (= (str "An Oxford comma list of apples, "
-          "oranges, "
-          "and pears.")
-     ((fn [[a b c]] (str "An Oxford comma list of " a ", " b ", " "and " c "."))
-      ["apples" "oranges" "pears"]))
+ "Whether in function definitions"
+ (= (str "An Oxford comma list of apples, "
+         "oranges, "
+         "and pears.")
+    ((fn [[a b c]] (str "An Oxford comma list of " a ", " b ", " "and " c "."))
+     ["apples" "oranges" "pears"]))
 
-  "Or in let expressions"
-  (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
-     (let [[first-name last-name & aliases]
-           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-       (apply str (interpose " " (apply list first-name last-name (interleave (repeat "aka") aliases))))))
+ "Or in let expressions"
+ (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
+    (let [[first-name last-name & aliases]
+          (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
+      (apply str (interpose " " (apply list first-name last-name (interleave (repeat "aka") aliases))))))
        ;; interleave (interleave c1 c2 & colls)
        ;; koan-engine.runner=> (interleave [:a :b :c] [10 20])
        ;; (:a 10 :b 20)
 
-  "You can regain the full argument if you like arguing"
-  (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
-     (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+ "You can regain the full argument if you like arguing"
+ (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
+    (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
+      {:original-parts full-name :named-parts {:first first-name :last last-name}}))
 
-  "Break up maps by key"
-  (= "123 Test Lane, Testerville, TX"
-     (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+ "Break up maps by key"
+ (= "123 Test Lane, Testerville, TX"
+    (let [{street-address :street-address, city :city, state :state} test-address]
+      (str street-address ", " city ", " state)))
 
-  "Or more succinctly"
-  (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+ "Or more succinctly"
+ (= "123 Test Lane, Testerville, TX"
+    (let [{:keys [street-address city state]} test-address]
+      (str street-address ", " city ", " state)))
 
-  "All together now!"
-  (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+ "All together now!"
+ (= "Test Testerson, 123 Test Lane, Testerville, TX"
+     ((fn [[f-name l-name]
+        {:keys [street-address city state]}]
+        (str f-name " " l-name ", " street-address ", " city ", " state))  ["Test" "Testerson"] test-address)))
+;; fn の定義 (fn name? [params*] exprs*)
+;; koan-engine.runner=> (map (fn [x] (+ x x)) (range 10))
+;; (0 2 4 6 8 10 12 14 16 18)
