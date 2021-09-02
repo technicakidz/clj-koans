e3e3e3b2ccb35ed209972325214d8101893bfcf6
technicakidz <y.technicakidz08@gmail.com>
2021-09-02 19:05:46 +0900

    [wip] datatypes まで

--- src/koans/17_atoms.clj
+++ src/koans/17_atoms.clj
@@ -5,29 +5,50 @@
 
 (meditations
   "Atoms are like refs"
-  (= __ @atomic-clock)
+  (= 0 @atomic-clock)
 
   "You can change at the swap meet"
-  (= __ (do
+  (= 1 (do
           (swap! atomic-clock inc)
           @atomic-clock))
+;; user=> (def vanibe (atom ()))
+;; #'user/vanibe
+;; user=> (swap! vanibe conj :rena)
+;; (:rena)
+;; user=> (swap! vanibe conj :risa)
+;; (:risa :rena)
+;; user=> (deref vanibe)
+;; (:risa :rena)
 
   "Keep taxes out of this: swapping requires no transaction"
   (= 5 (do
-         __
+         (swap! atomic-clock (partial + 4))
          @atomic-clock))
 
   "Any number of arguments might happen during a swap"
-  (= __ (do
+  (= 20 (do
           (swap! atomic-clock + 1 2 3 4 5)
+        ;;   (println atomic-clock)
           @atomic-clock))
 
+  
   "Atomic atoms are atomic"
-  (= __ (do
+  (= 20 (do
           (compare-and-set! atomic-clock 100 :fin)
           @atomic-clock))
+  ;; (compare-and-set! atom oldval newval)
+;; user=> (def a (atom 0))
+;; #'user/a
+;; user=> (compare-and-set! a 1 2)
+;; false
+;; user=> @a
+;; 0
+;; user=> (compare-and-set! a 0 100)
+;; true
+;; user=> @a
+;; 100
 
   "When your expectations are aligned with reality, things proceed that way"
   (= :fin (do
-            (compare-and-set! __ __ __)
+            (compare-and-set! atomic-clock 20 :fin)
             @atomic-clock)))
